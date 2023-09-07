f399c4947d59c3696ffa96642215a8d5274afd2d
Meby123B <meby123b@gmail.com>
2023-09-08 01:27:52 +0300

    Mouse Mapping

--- src/Game/Window.java
+++ src/Game/Window.java
@@ -1,13 +1,16 @@
 package Game;
 
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Window {
+public class Window implements MouseListener {
     public void powerOn(){
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("PacMeby");
+        window.addMouseListener(this);
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
@@ -19,6 +22,32 @@ public class Window {
 
 //        g gameloop = new GameLoop(player);
         gamePanel.startGameThread();
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("click");
+        System.out.println(e.getX()/ScreenSettings.tileSize+"|"+e.getY()/ScreenSettings.tileSize);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
